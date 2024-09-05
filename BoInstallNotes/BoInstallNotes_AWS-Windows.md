992edbdf02167b8b5bb81d3de65c48b9de4d1c0c
Bo Cordle <bo.cordle@rossvideo.com>
2024-09-05 15:14:22 -0400

    Installer updates

---
+++ BoInstallNotes/BoInstallNotes_AWS-Windows.md
@@ -0,0 +1,10 @@
+Install choco
+
+choco install gh
+choco install git
+choco install windows-sdk-10.1
+
+install vs build tools https://visualstudio.microsoft.com/visual-cpp-build-tools/
+
+choco install python
+
--- install_with_gpu_support.bat
+++ install_with_gpu_support.bat
@@ -1,2 +1,3 @@
-pip install torch==2.2.2+cu118 torchaudio==2.2.2+cu118 --index-url https://download.pytorch.org/whl/cu118
+@REM pip install torch==2.2.2+cu118 torchaudio==2.2.2+cu118 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements-gpu.txt
+pip install torch==2.3.1+cu121 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu121
